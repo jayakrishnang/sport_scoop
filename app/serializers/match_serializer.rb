4d633f553f80db95e9e540a4a00c5bc14eddacad
jayakrishnang <jkdesignlab@gmail.com>
2020-04-16 09:57:55 +0530

    Adding Images to Match list API

--- app/serializers/match_serializer.rb
+++ app/serializers/match_serializer.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class MatchSerializer < ActiveModel::Serializer
-  attributes :id, :number, :name, :start_time, :url, :status, :short_name
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :number, :name, :start_time, :url, :status, :short_name, :banner,
+             :description, :summary_message
+
+  def banner
+    ENV['HOME_PATH'] + rails_blob_path(object.banner, only_path: true) if object.banner.attached?
+  end
 end
